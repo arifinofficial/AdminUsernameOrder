a538edc69d953378d45c12ac600dbcb4a0004063
Arifin N <arifinofficial30@gmail.com>
2019-11-17 23:35:29 +0800

    observer cancel order

---
+++ Observer/Addcommentoncancelaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sunmedia\AdminUsernameOrder\Observer;
+
+class Addcommentoncancelaction implements \Magento\Framework\Event\ObserverInterface
+{
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+
+        $order = $observer->getData('order');
+        //$order->doSomething();
+        $authsession = \Magento\Framework\App\ObjectManager::getInstance()->create(\Magento\Backend\Model\Auth\Session::class);
+        $username = $authsession->getUser()->getUsername();
+
+
+        $notify = false;
+        $visible = false;
+        $history = $order->addStatusHistoryComment("Order canceled by" . " (" . $username . ")", $order->getStatus());
+        $history->setIsVisibleOnFront($visible);
+        $history->setIsCustomerNotified($notify);
+        $history->save();
+
+        return $this;
+    }
+}
